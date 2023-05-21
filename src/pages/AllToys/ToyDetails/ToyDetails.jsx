caba0ce847507710827c8bdbb7a611f0d46e883d
Mahin01 <Ashmahin4@gmail.com>
2023-05-21 15:10:51 +0600

    Single Toy details layout added

--- src/pages/AllToys/ToyDetails/ToyDetails.jsx
+++ src/pages/AllToys/ToyDetails/ToyDetails.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import { Button, Col, Container, Figure, Row } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 
 const ToyDetails = () => {
     const singleToyDetails = useLoaderData();
     console.log(singleToyDetails);
+    const {_id, ToyName, seller_name, seller_email,  SubCategory, ToyDetails, quantity, price, Photo_URL } = singleToyDetails;
     return (
-        <div>
-            
-        </div>
+        <Container className='banner'>
+            <Row className="my-5">
+
+            <Col xs={12} md={6}>
+                    <Figure>
+                        <Figure.Image className='rounded' src={Photo_URL} />
+                    </Figure>                 
+            </Col>
+
+                <Col xs={12} md={6}>
+                    <h5 className='product-title'> {ToyName} </h5>
+                    <h6 className="seller-name"> Seller: {seller_name} </h6>
+                    <h6 className="seller-name"> E-mail Address: {seller_email} </h6>
+                    <p className='product-description'> {ToyDetails} </p>
+                    <h6 className='quantity'> Available Quantity : {quantity} </h6>
+                    <h6 className='price'> Price : {price} </h6>
+                    <Button className='explore-button'>Add to Favorite</Button>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
