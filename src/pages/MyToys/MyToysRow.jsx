3f9f9b1ae570f58e7fe26de90388e897e4a603b9
Mahin01 <Ashmahin4@gmail.com>
2023-05-21 22:30:24 +0600

    My Toys Layout Completed

--- src/pages/MyToys/MyToysRow.jsx
+++ src/pages/MyToys/MyToysRow.jsx
@@ -2,36 +2,41 @@ import React from 'react';
 
 const MyToysRow = ({ addedToy }) => {
     const {_id, ToyName, seller_name, seller_email, quantity, price, Photo_URL } = addedToy;
+
+    // Handle Delete Data
+    const handleDelete = id => {
+       
+    }
+
     return (
         <div className='container'>
            <table className="table table-striped">
                     <thead>
                         <tr>
                             <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Email</th>
+                            <th>Toy Photo</th>
+                            <th>Toy Name</th>
+                            <th>Seller Name</th>
+                            <th>Seller Email</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <td>1</td>
-                            <td>Clark</td>
-                            <td>Kent</td>
-                            <td>clarkkent@mail.com</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Peter</td>
-                            <td>Parker</td>
-                            <td>peterparker@mail.com</td>
-                        </tr> 
-                        <tr>
-                            <td>3</td>
-                            <td>John</td>
-                            <td>Carter</td>
-                            <td>johncarter@mail.com</td>
-                        </tr>                   
+                            <td>{Photo_URL}</td>
+                            <td>{ToyName}</td>
+                            <td>{seller_name}</td>
+                            <td>{seller_email}</td>
+                            <td>{quantity}</td>
+                            <td>{price}</td>
+                            <td>
+                                <button className='btn btn-primary me-2'>Update</button>
+                                <button onClick={() => handleDelete(_id)} className='btn btn-danger'>Delete</button>
+                            </td>
+                        </tr>                         
                     </tbody>
                 </table>
         </div>
